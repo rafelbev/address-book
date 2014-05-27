d9ac537f90c992c4525e6b32de9bc02c4994ef2b
Raphael Borg Ellul Vincenti <raphael@neo-tix.com>
2014-05-27 15:59:46 +0200

    Refactor mocked DOB to constants

--- src/test/java/com/springapp/batch/CompareEntryTest.java
+++ src/test/java/com/springapp/batch/CompareEntryTest.java
@@ -13,13 +13,16 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class CompareEntryTest {
 
+    public static final String BILL_DOB = "1970-01-08T12:30:49+05:30";
+    public static final String PAUL_DOB = "1970-01-09T15:30:49+05:30";
+
     @Test
     public void testJob() throws Exception {
         BookEntry bill = mock(BookEntry.class);
         BookEntry paul = mock(BookEntry.class);
 
-        when(bill.getDateOfBirth()).thenReturn(DateTime.parse("1970-01-08T12:30:49+05:30"));
-        when(paul.getDateOfBirth()).thenReturn(DateTime.parse("1970-01-09T15:30:49+05:30"));
+        when(bill.getDateOfBirth()).thenReturn(DateTime.parse(BILL_DOB));
+        when(paul.getDateOfBirth()).thenReturn(DateTime.parse(PAUL_DOB));
 
         Assert.assertEquals(1, bill.compareTo(paul));
     }
