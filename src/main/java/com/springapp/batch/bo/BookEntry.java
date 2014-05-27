af24907db753a132784afe3528b731d41f46e7f6
Raphael Borg Ellul Vincenti <raphael@neo-tix.com>
2014-05-27 15:32:20 +0200

    Add more types of constructors for BookEntry

--- src/main/java/com/springapp/batch/bo/BookEntry.java
+++ src/main/java/com/springapp/batch/bo/BookEntry.java
@@ -1,16 +1,30 @@
 package com.springapp.batch.bo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 /**
  * BO used to describe rows found insider the AddressBook
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookEntry {
 
     private String name;
     private Gender sex;
     private DateTime dateOfBirth;
 
+    public BookEntry(String name, String sex, String dateOfBirth) {
+        this();
+        setName(name);
+        setSex(Gender.customValueOf(sex));
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("dd/MM/yy");
+        DateTime parsedDateOfBirth = dateTimeFormatter.parseDateTime(dateOfBirth);
+        setDateOfBirth(parsedDateOfBirth);
+    }
 }
