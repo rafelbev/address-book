e8f0b179f4b144ca37168d0d33043be6115bfa60
Raphael Borg Ellul Vincenti <raphael@neo-tix.com>
2014-06-14 13:36:45 +0200

    Autowire the fieldset mappers to be used in the configuration

--- src/main/java/com/springapp/batch/config/MapperConfiguration.java
+++ src/main/java/com/springapp/batch/config/MapperConfiguration.java
@@ -3,19 +3,17 @@ package com.springapp.batch.config;
 import com.springapp.batch.mapper.PersonFieldSetMapper;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.batch.item.file.transform.LineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MapperConfiguration {
 
-    @Bean
-    public FieldSetMapper personFieldsetMapper() {
-        return new PersonFieldSetMapper();
-    }
+    @Autowired
+    private PersonFieldSetMapper personFieldSetMapper;
 
     @Bean
     public LineTokenizer tokenizer() {
@@ -25,7 +23,7 @@ public class MapperConfiguration {
     @Bean
     public LineMapper lineMapper() {
         DefaultLineMapper mapper = new DefaultLineMapper();
-        mapper.setFieldSetMapper(personFieldsetMapper());
+        mapper.setFieldSetMapper(personFieldSetMapper);
         mapper.setLineTokenizer(tokenizer());
         return mapper;
     }
--- src/main/java/com/springapp/batch/mapper/BasicPersonFieldSetMapper.java
+++ src/main/java/com/springapp/batch/mapper/BasicPersonFieldSetMapper.java
@@ -4,8 +4,10 @@ import com.springapp.batch.bo.BasicPerson;
 import com.springapp.batch.bo.Gender;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.BindException;
 
+@Component
 public class BasicPersonFieldSetMapper implements FieldSetMapper<BasicPerson> {
     @Override
     public BasicPerson mapFieldSet(FieldSet fieldSet) throws BindException {
