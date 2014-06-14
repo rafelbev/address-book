8918249fb06d0d100fd597e06036ecabc4c11fe6
Raphael Borg Ellul Vincenti <raphael@neo-tix.com>
2014-06-14 13:24:04 +0200

    Create a mapper for the simpler BasicPerson BO

---
+++ src/main/java/com/springapp/batch/mapper/BasicPersonFieldSetMapper.java
@@ -0,0 +1,19 @@
+package com.springapp.batch.mapper;
+
+import com.springapp.batch.bo.BasicPerson;
+import com.springapp.batch.bo.Gender;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class BasicPersonFieldSetMapper implements FieldSetMapper<BasicPerson> {
+    @Override
+    public BasicPerson mapFieldSet(FieldSet fieldSet) throws BindException {
+        Gender sex = Gender.customValueOf(fieldSet.readString(1));
+
+        BasicPerson person = new BasicPerson();
+        person.setSex(sex);
+
+        return person;
+    }
+}
